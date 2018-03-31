343a7f37f8906c7fb0990957be330b53bb08a46a
Wang Chengming <wangchengmingbbs@126.com>
2018-03-31 20:41:47 +0800

    add A2

---
+++ Assignment2/connected_component.m
@@ -0,0 +1,35 @@
+% connected_component.m contains the implementation of the main routine for Question 1 in Assignment 2. 
+% This function search for all connected component on the input image. It should output the total number of
+% connected components, number of pixels in each connected component and
+% display the largest connected component. Note that you are not allow to
+% use the bwlabel/bwlabeln/bwconncomp matlab built-in function in this
+% question.
+
+function connected_component()
+    %Read the input image and convert it to binary image (0,1)
+    IM = imread('cc_image.jpg');
+    BW = im2bw(IM);
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% TODO_1: Search for all connected components with connectivity equals
+	% to 8 and output the result to the command window in following format:
+    % There are total {number of regions} region(s) :
+    % Region 1, no. of pixels = {number of pixels}
+    % Region 2, no. of pixels = {number of pixels}
+    % ...
+    
+	
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	% TODO_2: Find the largest connected component in binary format (0,1).
+	% L_CC = ??
+    
+    
+	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    figure;
+    subplot(1,2,1);imshow(BW);title('Input image');
+    subplot(1,2,2);imshow(L_CC);title('Largest connected component');
+    
+end
+
