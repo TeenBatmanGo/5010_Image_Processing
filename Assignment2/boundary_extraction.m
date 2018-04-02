e2426b94c0aea79429fd6aef43c934f5e4c45b7b
Wang Chengming <wangchengmingbbs@126.com>
2018-04-02 16:13:00 +0800

    Update

--- Assignment2/boundary_extraction.m
+++ Assignment2/boundary_extraction.m
@@ -16,12 +16,33 @@ function boundary_extraction()
     % TODO_1: Apply erosion on the image
     % img_erode = ??
     
+    padded_img = padarray(img, [1 1]);
+    [sizeX, sizeY] = size(img);
+    img_erode = zeros(size(padded_img));
+
+    for i=2:sizeX
+        for j=2:sizeY
+            flag = 1;
+            for k=-1:1
+                for p=-1:1
+                    flag = flag & padded_img(i+k, j+p);
+                end
+            end
+            img_erode(i, j) = flag;
+        end
+    end
+
+    img_erode = img_erode(2:sizeX+1, 2:sizeY+1);
+
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % TODO_1: Apply erosion on the image
     % img_boundary = ??
     
+    img_boundary = img - img_erode;
+
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     % Display the input image
